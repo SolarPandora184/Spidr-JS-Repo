eb45a38c3d820a2f43424bbb3ee942c59d7c2df1
TC Weed Abatement <Kendrick.uhles@cawgcap.org>
2026-06-02 12:28:39 -0700

    Update 4legged-bug.js

--- 4legged-bug.js
+++ 4legged-bug.js
@@ -1,10 +1,8 @@
-//Ignore servo commands added for digital testing purposes.//
-
 let Status = 0
 basic.forever(function () {
     if (input.soundLevel() >= 100 && input.soundLevel() < 200) {
         Status = 1
-    } else if (false) {
+    } else if (input.soundLevel() > 60 && input.soundLevel() < 100) {
         Status = 2
     } else {
         Status = 0
@@ -15,36 +13,31 @@ basic.forever(function () {
         30,
         1
         )
-        servos.P0.run(50)
-        basic.pause(1000)
+        basic.pause(3000)
         RoboticsWorkshop.DDMmotor2(
         MotorChannel.MotorA,
         0,
         0
         )
-        servos.P0.run(0)
         basic.pause(1000)
     } else if (Status == 2) {
         RoboticsWorkshop.DDMmotor2(
         MotorChannel.MotorA,
         0,
-        1
+        0
         )
-        servos.P0.run(50)
         for (let index = 0; index < 2; index++) {
             RoboticsWorkshop.DDMmotor2(
             MotorChannel.MotorB,
             60,
             0
             )
-            servos.P0.run(0)
             basic.pause(1000)
             RoboticsWorkshop.DDMmotor2(
             MotorChannel.MotorB,
             60,
             1
             )
-            servos.P0.run(50)
             basic.pause(1000)
         }
         RoboticsWorkshop.DDMmotor2(
@@ -52,20 +45,17 @@ basic.forever(function () {
         0,
         0
         )
-        servos.P0.run(0)
         basic.pause(1000)
     } else {
         RoboticsWorkshop.DDMmotor2(
         MotorChannel.MotorA,
         0,
-        1
+        0
         )
-        servos.P0.run(50)
         RoboticsWorkshop.DDMmotor2(
         MotorChannel.MotorB,
         0,
-        1
+        0
         )
-        servos.P0.run(0)
     }
 })
